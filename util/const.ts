aacd85051871c394a29927e729e8bda3971a4dfd
Matsu <50922910+mtsml@users.noreply.github.com>
2024-01-11 18:51:05 +0900

    レイアウト修正 (#33)

--- util/const.ts
+++ util/const.ts
@@ -1,2 +1,4 @@
 export const SERVICE_NAME = "楽曲10選まとめ";
 export const SERVICE_URL = "https://10sen.wiki/";
+export const CONTACT_US_X_ID = "@pine_nm";
+export const CONTACT_US_X_LINK = "https://x.com/pine_nm";
