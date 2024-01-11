858b1fbb67ed5e7e3463f269e0ed2917861d13de
matsu <50922910+mtsml@users.noreply.github.com>
2024-01-11 15:57:41 +0900

    CSSを変数に

--- components/Accordion/Accordion.tsx
+++ components/Accordion/Accordion.tsx
@@ -6,9 +6,10 @@ import styles from "./Accordion.module.css";
 type AccordionProps = {
   header: ReactNode;
   children: ReactNode;
+  type?: "information" | "warning";
 }
 
-const Accordion = ({ header, children }: AccordionProps) => {
+const Accordion = ({ header, type = "information", children }: AccordionProps) => {
   const ref = useRef<HTMLDivElement>(null);
   const [height, setHeight] = useState("0px");
   const [isOpen, setIsOpen] = useState(false);
@@ -20,7 +21,7 @@ const Accordion = ({ header, children }: AccordionProps) => {
   }, [isOpen]);
 
   return (
-    <div className={styles.accordion}>
+    <div className={`${styles.accordion} ${styles[type]}`}>
       <div
         className={styles["accordion-header"]}
         onClick={() => setIsOpen(!isOpen)}
